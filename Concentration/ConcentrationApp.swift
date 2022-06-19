5ca2d94674087a56c54fb700f236dc9170cdeeb6
Oksenoyt <a.oksenoyt@gmail.com>
2022-06-19 11:53:55 +0300

    Initial Commit

---
+++ Concentration/ConcentrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcentrationApp.swift
+//  Concentration
+//
+//  Created by Elenka on 19.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct ConcentrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
